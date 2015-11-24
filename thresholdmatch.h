e9781b363e0bd3dbaaab59fe582f427ecaf8f733
Fredrik Sävje <fred_gh@savje.com>
2015-11-23 22:13:32 -0800

    Add base types

---
+++ thresholdmatch.h
@@ -0,0 +1,13 @@
+#ifndef THM_THRESHOLD_HG
+#define THM_THRESHOLD_HG
+
+// Vertex ID type
+typedef unsigned long thm_Vid;
+
+// Vertex ID type
+typedef unsigned long thm_Arcref;
+
+// Block label type
+typedef unsigned long thm_Blklab;
+
+#endif
