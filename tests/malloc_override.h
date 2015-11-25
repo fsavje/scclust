5490f1c7ca69ff228652da7dbf6aadfb94feb272
Fredrik Sävje <fred_gh@savje.com>
2015-11-24 22:36:50 -0800

    Split test helper functions

---
+++ tests/malloc_override.h
@@ -0,0 +1,9 @@
+#ifndef UT_THM_MALLOC_OVERRIDE_HG
+#define UT_THM_MALLOC_OVERRIDE_HG
+
+#include <stdlib.h>
+#define malloc(size) _test_malloc(size, __FILE__, __LINE__)
+#define calloc(num, size) _test_calloc(num, size, __FILE__, __LINE__)
+#define free(ptr) _test_free(ptr, __FILE__, __LINE__)
+
+#endif
--- tests/test_digraph.c
+++ tests/test_digraph.c
@@ -3,46 +3,12 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
-#include <stdlib.h>
-#define malloc(size) _test_malloc(size, __FILE__, __LINE__)
-#define calloc(num, size) _test_calloc(num, size, __FILE__, __LINE__)
-#define free(ptr) _test_free(ptr, __FILE__, __LINE__)
+#include "malloc_override.h"
 
 #include "../digraph.c"
 
+#include "assert_digraph.h"
 
-
-void assert_object_digraph(thm_Digraph* test_dg, thm_Vid vertices) {
-	assert_int_equal(test_dg->vertices, vertices);
-	assert_non_null(test_dg->tail_ptr);
-	if (test_dg->max_arcs > 0) assert_non_null(test_dg->head);
-}
-
-void assert_mem_digraph(thm_Digraph* test_dg) {
-	for (size_t i = 0; i <= test_dg->vertices; ++i) test_dg->tail_ptr[i] = 1;
-	for (size_t i = 0; i < test_dg->max_arcs; ++i) test_dg->head[i] = 1;
-}
-
-void assert_valid_digraph(thm_Digraph* test_dg, thm_Vid vertices) {
-	assert_object_digraph(test_dg, vertices);
-	assert_int_equal(test_dg->tail_ptr[0], 0);
-	assert_true(test_dg->tail_ptr[vertices] <= test_dg->max_arcs);
-	for (size_t i = 0; i < vertices; ++i) assert_true(test_dg->tail_ptr[i] <= test_dg->tail_ptr[i + 1]);
-	for (size_t i = 0; i < test_dg->tail_ptr[vertices]; ++i) assert_true(test_dg->head[i] < vertices);
-}
-
-void assert_empty_digraph(thm_Digraph* test_dg, thm_Vid vertices) {
-	assert_valid_digraph(test_dg, vertices);
-	assert_int_equal(test_dg->tail_ptr[vertices], 0);
-}
-
-void assert_balanced_digraph(thm_Digraph* test_dg, thm_Vid vertices, thm_Vid arcs_per_vertex) {
-	assert_valid_digraph(test_dg, vertices);
-	assert_true(vertices * arcs_per_vertex <= test_dg->max_arcs);
-	for (size_t i = 0; i <= vertices; ++i) {
-		assert_int_equal(test_dg->tail_ptr[i], i * arcs_per_vertex);
-	}
-}
 
 void ut_thm_Digraph(void** state) {
 	size_t digraph_size = sizeof(thm_Vid) + sizeof(thm_Arcref*) + sizeof(thm_Arcref) + sizeof(thm_Vid*);
