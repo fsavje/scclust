7e06dbcd842291895bb7fca0c4e4658b06cabb5f
Fredrik Sävje <fred_gh@savje.com>
2015-11-25 23:52:52 -0800

    Change digraph init

--- digraph.c
+++ digraph.c
@@ -1,43 +1,74 @@
 #include "digraph.h"
+
 #include <stdlib.h>
 
-thm_Digraph thm_init_digraph(const thm_Vid vertices) {
+thm_Digraph thm_init_digraph(const thm_Vid vertices, const thm_Arcref max_arcs) {
 	thm_Digraph dg = {
-		.vertices = vertices,
-		.tail_ptr = malloc(sizeof(thm_Arcref[vertices + 1])),
-		.max_arcs = 0,
 		.head = NULL,
+		.tail_ptr = malloc(sizeof(thm_Arcref[vertices + 1])),
+		.vertices = vertices,
+		.max_arcs = max_arcs,
+		.valid = true,
 	};
+
+	if (!dg.tail_ptr) return (thm_Digraph) { 0 };
+
+	if (max_arcs > 0) {
+		dg.head = malloc(sizeof(thm_Vid[max_arcs]));
+		if (!dg.head) thm_free_digraph(&dg);
+	}
 
 	return dg;
 }
 
+void thm_free_digraph(thm_Digraph* const dg) {
+	if (dg) {
+		free(dg->head);
+		free(dg->tail_ptr);
+		*dg = (thm_Digraph) { 0 };
+	}
+}
+
+bool thm_change_digraph_head_size(thm_Digraph* const dg, const thm_Arcref new_max_arcs) {
+	if (dg->max_arcs == new_max_arcs) return true;
+	if (dg->tail_ptr[dg->vertices] > new_max_arcs) return false;
+
+	if (new_max_arcs == 0) {
+		free(dg->head);
+		dg->head = NULL;
+		dg->max_arcs = 0;
+	} else {
+		thm_Vid* tmp_ptr = realloc(dg->head, sizeof(thm_Vid[new_max_arcs]));
+		if (!tmp_ptr) return false;
+		dg->head = tmp_ptr;
+		dg->max_arcs = new_max_arcs;
+	}
+
+	return true;
+}
+
 thm_Digraph thm_empty_digraph(const thm_Vid vertices, const thm_Arcref max_arcs) {
-	thm_Digraph dg = thm_init_digraph(vertices);
+	thm_Digraph dg = thm_init_digraph(vertices, max_arcs);
+	if (!dg.valid) return dg;
+	
 	for (thm_Vid v = 0; v <= vertices; ++v) dg.tail_ptr[v] = 0;
-	dg.max_arcs = max_arcs;
-	dg.head = malloc(sizeof(thm_Vid[max_arcs]));
 
 	return dg;
 }
 
 thm_Digraph thm_balanced_digraph(const thm_Vid vertices, const thm_Vid arcs_per_vertex, thm_Vid* const heads) {
-	thm_Digraph dg = thm_init_digraph(vertices);
+	thm_Digraph dg = thm_init_digraph(vertices, 0);
+	if (!dg.valid) return dg;
+
+	dg.max_arcs = vertices * arcs_per_vertex;
+	dg.head = heads;
 	for (thm_Vid v = 0; v <= vertices; ++v) {
 		dg.tail_ptr[v] = v * arcs_per_vertex;
 	}
-	dg.max_arcs = vertices * arcs_per_vertex;
-	dg.head = heads;
-
+	
 	return dg;
 }
 
-void thm_free_digraph(thm_Digraph* const a) {
-	if (a) {
-		free(a->head);
-		free(a->tail_ptr);
-		*a = (thm_Digraph) { 0 };
-	}
 }
 
 //thm_Digraph thm_add_digraphs(const thm_Digraph* const a, const thm_Digraph* const b){
--- digraph.h
+++ digraph.h
@@ -6,25 +6,29 @@
 
 typedef struct thm_Digraph thm_Digraph;
 struct thm_Digraph {
-	// Number of vertices in graph: vertices < MAX(thm_Vid).
-	thm_Vid vertices;
+	// Array of unsigned longs: [0, ..., max_arcs - 1].
+	// Contains the head of each arc, indicated by vertex ID.
+	thm_Vid* head;
 
 	// Array of pointers: [0, ..., vertices].
 	// For arcs for which vertex i is the tail, head[tail_ptr[i]] is the head of the first arc and head[tail_ptr[i+1] - 1] is the last arc.
 	thm_Arcref* tail_ptr;
 
+	// Number of vertices in graph: vertices < MAX(thm_Vid).
+	thm_Vid vertices;
+
 	// Maximum number of arcs in graph: max_arcs <= MAX(thm_Arcref).
 	thm_Arcref max_arcs;
 
-	// Array of unsigned longs: [0, ..., max_arcs - 1].
-	// Contains the head of each arc, indicated by vertex ID.
-	thm_Vid* head;
+	// Valid and initialized (head may be NULL if max_arcs == 0)
+	bool valid;
 };
 
-thm_Digraph thm_init_digraph(thm_Vid vertices);
+thm_Digraph thm_init_digraph(thm_Vid vertices, thm_Arcref max_arcs);
+void thm_free_digraph(thm_Digraph* dg);
+bool thm_change_digraph_head_size(thm_Digraph* dg, thm_Arcref new_max_arcs);
 thm_Digraph thm_empty_digraph(thm_Vid vertices, thm_Arcref max_arcs);
 thm_Digraph thm_balanced_digraph(thm_Vid vertices, thm_Vid arcs_per_vertex, thm_Vid* heads);
-void thm_free_digraph(thm_Digraph* a);
 
 //thm_Digraph thm_add_digraphs(const thm_Digraph* a, const thm_Digraph* b);
 //fm_digraph fm_transpose(fm_digraph a);
