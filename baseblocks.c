5c36b2fddbe16628a302524737fac43d131de01e
Fredrik Sävje <fred_gh@savje.com>
2015-11-27 23:39:47 -0800

    Make minor changes

--- baseblocks.c
+++ baseblocks.c
@@ -1,22 +1,40 @@
 #include "baseblocks.h"
-#include <stdlib.h>
-#include "digraph.h"
 
-static void ithm_fs_lexical(const thm_Digraph* nng, thm_Blocking* blocking);
+#include <stdlib.h>
+
+static void ithm_findseeds_lexical(const thm_Digraph* nng, thm_Blocking* blocking);
+static inline void ithm_set_seed(thm_Vid s, const thm_Vid* a, const thm_Vid* a_stop, thm_Blocking* blocking);
+
 
 thm_Blocking thm_base_blocks(const thm_Digraph* const nng, const thm_SeedMethod sm) {
+	if (!nng || !nng->valid) return (thm_Blocking) { 0 };
+
 
 	thm_Blocking blocking = {
-		.vertices = nng->num_vertices,
+		.vertices = nng->vertices,
 		.blocks = 0,
-		.assigned = calloc(nng->num_vertices, sizeof(bool)),
-		.seed = calloc(nng->num_vertices, sizeof(bool)),
-		.block_label = malloc(sizeof(thm_Blklab[nng->num_vertices]))
+		.assigned = calloc(nng->vertices, sizeof(bool)),
+		.seed = calloc(nng->vertices, sizeof(bool)),
+		.block_label = malloc(sizeof(thm_Blklab[nng->vertices])),
+		.valid = true,
 	};
 
-	ithm_fs_lexical(nng, &blocking);
+	if (sm == lexical) {
+		ithm_findseeds_lexical(nng, &blocking);
+	}
 
 	return blocking;
+}
+
+static void ithm_findseeds_lexical(const thm_Digraph* const nng, thm_Blocking* const blocking) {
+	for (size_t c = 0; c < nng->vertices; ++c) {
+		if (!blocking->assigned[c]) {
+			const thm_Vid* a = nng->head + nng->tail_ptr[c];
+			const thm_Vid* const a_stop = nng->head + nng->tail_ptr[c + 1];
+			for (; a != a_stop && !blocking->assigned[*a]; ++a) { }
+			if (a == a_stop) ithm_set_seed(c, (nng->head + nng->tail_ptr[c]), a_stop, blocking);
+		}
+	}
 }
 
 static inline void ithm_set_seed(const thm_Vid s, const thm_Vid* a, const thm_Vid* const a_stop, thm_Blocking* const blocking) {
@@ -28,15 +46,4 @@ static inline void ithm_set_seed(const thm_Vid s, const thm_Vid* a, const thm_Vi
 		blocking->block_label[*a] = blocking->blocks;
 	}
 	++(blocking->blocks);
-}
-
-static void ithm_fs_lexical(const thm_Digraph* const nng, thm_Blocking* const blocking) {
-	for (size_t c = 0; c < nng->num_vertices; ++c) {
-		if (!blocking->assigned[c]) {
-			const thm_Vid* a = nng->head + nng->tail_ptr[c];
-			const thm_Vid* const a_stop = nng->head + nng->tail_ptr[c + 1];
-			for (; a != a_stop && !blocking->assigned[*a]; ++a) { }
-			if (a == a_stop) ithm_set_seed(c, (nng->head + nng->tail_ptr[c]), a_stop, blocking);
-		}
-	}
 }
--- baseblocks.h
+++ baseblocks.h
@@ -3,9 +3,10 @@
 
 #include <stddef.h>
 #include <stdbool.h>
-#include "config.h"
 
-typedef struct thm_Digraph thm_Digraph; // see "digraph.h"
+#include "config.h"
+#include "digraph.h"
+
 
 typedef struct thm_Blocking thm_Blocking;
 struct thm_Blocking {
@@ -14,10 +15,11 @@ struct thm_Blocking {
 	bool* assigned;
 	bool* seed;
 	thm_Blklab* block_label;
+	bool valid;
 };
 
 typedef enum thm_SeedMethod thm_SeedMethod;
-enum thm_SeedMethod { lexical, order_outwards, order_second_power, simulated_annealing, maximum_is };
+enum thm_SeedMethod { lexical, order_inwards_arcs, order_second_power, simulated_annealing, maximum_is };
 
 thm_Blocking thm_base_blocks(const thm_Digraph* nng, thm_SeedMethod sm);
 
