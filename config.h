1faf4681396ca217899ace40a2b60b04a2a8e3c3
Fredrik Sävje <fred_gh@savje.com>
2015-11-27 23:40:06 -0800

    Move typedef to digraph header

--- config.h
+++ config.h
@@ -1,16 +1,8 @@
 #ifndef THM_CONFIG_HG
 #define THM_CONFIG_HG
 
-// Vertex ID type
-// # of vertices < MAX(thm_Vid)
-typedef unsigned long thm_Vid;
-
-// Arc reference type
-// # of arcs in any digraph <= MAX(thm_Arcref)
-typedef unsigned long thm_Arcref;
-
-// Block label type
-// # of blocks < MAX(thm_Blklab)
-typedef unsigned long thm_Blklab;
+// Block label type (must be unsigned)
+// # of blocks <= MAX(thm_Blklab)
+typedef uint32_t thm_Blklab;
 
 #endif
