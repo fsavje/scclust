be4ec409ce58ed0f40d17959233c50b03a4e8673
Fredrik Sävje <fred_gh@savje.com>
2015-11-27 23:43:18 -0800

    Add multiplication and transpose

--- digraph.c
+++ digraph.c
@@ -2,13 +2,13 @@
 
 #include <stdlib.h>
 
+
 thm_Digraph thm_init_digraph(const thm_Vid vertices, const thm_Arcref max_arcs) {
 	thm_Digraph dg = {
-		.head = NULL,
-		.tail_ptr = malloc(sizeof(thm_Arcref[vertices + 1])),
 		.vertices = vertices,
 		.max_arcs = max_arcs,
-		.valid = true,
+		.head = NULL,
+		.tail_ptr = malloc(sizeof(thm_Arcref[vertices + 1])),
 	};
 
 	if (!dg.tail_ptr) return (thm_Digraph) { 0 };
@@ -21,6 +21,7 @@ thm_Digraph thm_init_digraph(const thm_Vid vertices, const thm_Arcref max_arcs) 
 	return dg;
 }
 
+
 void thm_free_digraph(thm_Digraph* const dg) {
 	if (dg) {
 		free(dg->head);
@@ -29,7 +30,9 @@ void thm_free_digraph(thm_Digraph* const dg) {
 	}
 }
 
+
 bool thm_change_digraph_head_size(thm_Digraph* const dg, const thm_Arcref new_max_arcs) {
+	if (!dg || !dg->tail_ptr) return false;
 	if (dg->max_arcs == new_max_arcs) return true;
 	if (dg->tail_ptr[dg->vertices] > new_max_arcs) return false;
 
@@ -47,18 +50,34 @@ bool thm_change_digraph_head_size(thm_Digraph* const dg, const thm_Arcref new_ma
 	return true;
 }
 
+
 thm_Digraph thm_empty_digraph(const thm_Vid vertices, const thm_Arcref max_arcs) {
 	thm_Digraph dg = thm_init_digraph(vertices, max_arcs);
-	if (!dg.valid) return dg;
+	if (!dg.tail_ptr) return dg;
 	
 	for (thm_Vid v = 0; v <= vertices; ++v) dg.tail_ptr[v] = 0;
 
 	return dg;
 }
 
+
+thm_Digraph thm_identity_digraph(const thm_Vid vertices) {
+	thm_Digraph dg = thm_init_digraph(vertices, vertices);
+	if (!dg.tail_ptr) return dg;
+	
+	for (thm_Vid v = 0; v < vertices; ++v) {
+		dg.tail_ptr[v] = v;
+		dg.head[v] = v;
+	}
+	dg.tail_ptr[vertices] = vertices;
+
+	return dg;
+}
+
+
 thm_Digraph thm_balanced_digraph(const thm_Vid vertices, const thm_Vid arcs_per_vertex, thm_Vid* const heads) {
 	thm_Digraph dg = thm_init_digraph(vertices, 0);
-	if (!dg.valid) return dg;
+	if (!dg.tail_ptr) return dg;
 
 	dg.max_arcs = vertices * arcs_per_vertex;
 	dg.head = heads;
@@ -69,59 +88,300 @@ thm_Digraph thm_balanced_digraph(const thm_Vid vertices, const thm_Vid arcs_per_
 	return dg;
 }
 
-thm_Digraph thm_add_two_digraphs(const thm_Digraph* const dg_a, const thm_Digraph* const dg_b, const bool remove_duplicates) {
-	const thm_Digraph* dgs_ab[2] = {dg_a, dg_b};
-	return thm_add_digraphs(2, dgs_ab, remove_duplicates);
+
+thm_Digraph thm_copy_digraph(const thm_Digraph* const dg) {
+	if (!dg || !dg->tail_ptr) return (thm_Digraph) { 0 };
+	if (dg->vertices == 0) return thm_empty_digraph(0, 0);
+
+	thm_Digraph dg_out = thm_init_digraph(dg->vertices, dg->tail_ptr[dg->vertices]);
+	if (!dg_out.tail_ptr) return dg_out;
+
+	for (thm_Vid v = 0; v <= dg->vertices; ++v) dg_out.tail_ptr[v] = dg->tail_ptr[v];
+	for (thm_Arcref a = 0; a < dg->tail_ptr[dg->vertices]; ++a) dg_out.head[a] = dg->head[a];
+
+	return dg_out;
 }
 
-thm_Digraph thm_add_digraphs(const size_t num_dgs, const thm_Digraph** const dgs, const bool remove_duplicates) {
-	if (num_dgs == 0) return (thm_Digraph) { 0 };
-	if (!dgs[0] || !dgs[0]->valid) return (thm_Digraph) { 0 };
+
+thm_Digraph thm_digraph_union_two(const thm_Digraph* const dg_a, const thm_Digraph* const dg_b) {
+	const thm_Digraph* dgs_ab[2] = {dg_a, dg_b};
+	return thm_digraph_union(2, dgs_ab);
+}
+
+
+thm_Digraph thm_digraph_union(const size_t num_dgs, const thm_Digraph** const dgs) {
+	if (num_dgs == 0) return thm_empty_digraph(0, 0);
+	if (!dgs || !dgs[0]) return (thm_Digraph) { 0 };
+
 	thm_Vid vertices = dgs[0]->vertices;
-	thm_Arcref max_arcs = dgs[0]->tail_ptr[vertices];
-	for (size_t i = 1; i < num_dgs; ++i) {
-		if (!dgs[i] || !dgs[i]->valid || dgs[i]->vertices != vertices) return (thm_Digraph) { 0 };
-		max_arcs += dgs[i]->tail_ptr[vertices];
+	thm_Arcref out_max_arcs = 0;
+	for (size_t i = 0; i < num_dgs; ++i) {
+		if (!dgs[i] || !dgs[i]->tail_ptr || dgs[i]->vertices != vertices) return (thm_Digraph) { 0 };
+		out_max_arcs += dgs[i]->tail_ptr[vertices];
 	}
 
-	if (max_arcs == 0) return thm_empty_digraph(vertices, 0);
+	thm_Vid* row_markers = malloc(sizeof(thm_Vid[vertices]));
+	if (!row_markers) return (thm_Digraph) { 0 };
 
-	thm_Digraph dg_out = thm_init_digraph(vertices, max_arcs);
-	if (!dg_out.valid) return dg_out;
+	thm_Digraph dg_out = thm_init_digraph(vertices, out_max_arcs);
 
-	thm_Arcref out_arc_write = 0;
-	dg_out.tail_ptr[0] = 0;
+	if (!dg_out.tail_ptr) {
+		// Could not allocate digraph with `row_markers' arcs.
+		// Do correct (but slow) memory calculation and try again.
 
-	for (thm_Vid v = 0; v < vertices; ++v) {
+		out_max_arcs = 0;
+		for (thm_Vid v = 0; v < vertices; ++v) row_markers[v] = THM_VID_MAX;
 
-		thm_Vid* prev_add_stop = NULL;
-
-		for (size_t i = 0; i < num_dgs; ++i) {
-
-			thm_Vid* toadd_stop = dgs[i]->head + dgs[i]->tail_ptr[v + 1];
-
-			for (thm_Vid* arc_toadd = dgs[i]->head + dgs[i]->tail_ptr[v];
-					arc_toadd != toadd_stop; ++arc_toadd) {
-				dg_out.head[out_arc_write] = *arc_toadd;
-				++out_arc_write;
-				if (prev_add_stop) {
-					for (thm_Vid* arc_prevadd = dg_out.head + dg_out.tail_ptr[v];
-							arc_prevadd != prev_add_stop; ++arc_prevadd) {
-						if (*arc_prevadd == *arc_toadd) {
-							--out_arc_write;
-							break;
-						}
+		for (thm_Vid v = 0; v < vertices; ++v) {
+			for (size_t i = 0; i < num_dgs; ++i) {
+				for (thm_Vid* arc_toadd = dgs[i]->head + dgs[i]->tail_ptr[v];
+						arc_toadd != dgs[i]->head + dgs[i]->tail_ptr[v + 1];
+						++arc_toadd) {
+					if (row_markers[*arc_toadd] != v) {
+						row_markers[*arc_toadd] = v;
+						++out_max_arcs;
 					}
 				}
 			}
+		}
 
-			if (remove_duplicates) prev_add_stop = dg_out.head + out_arc_write;
+		dg_out = thm_init_digraph(vertices, out_max_arcs);
+	}
+
+	// Try again. If fail, give up.
+	if (!dg_out.tail_ptr) {
+		free(row_markers);
+		return dg_out;
+	}
+
+	dg_out.tail_ptr[0] = 0;
+	thm_Arcref out_arc_write = 0;
+	for (thm_Vid v = 0; v < vertices; ++v) row_markers[v] = THM_VID_MAX;
+
+	for (thm_Vid v = 0; v < vertices; ++v) {
+		for (size_t i = 0; i < num_dgs; ++i) {
+			for (thm_Vid* arc_toadd = dgs[i]->head + dgs[i]->tail_ptr[v];
+					arc_toadd != dgs[i]->head + dgs[i]->tail_ptr[v + 1];
+					++arc_toadd) {
+				if (row_markers[*arc_toadd] != v) {
+					row_markers[*arc_toadd] = v;
+					dg_out.head[out_arc_write] = *arc_toadd;
+					++out_arc_write;
+				}
+			}
 		}
 
 		dg_out.tail_ptr[v + 1] = out_arc_write;
 	}
 
-	if (remove_duplicates) thm_change_digraph_head_size(&dg_out, out_arc_write);
+	free(row_markers);
+
+	thm_change_digraph_head_size(&dg_out, out_arc_write);
 
 	return dg_out;
 }
+
+
+thm_Digraph thm_digraph_transpose(const thm_Digraph* const dg) {
+	if (!dg || !dg->tail_ptr) return (thm_Digraph) { 0 };
+	if (dg->vertices == 0) return thm_empty_digraph(0, 0);
+
+	thm_Vid* row_count = calloc(dg->vertices + 1, sizeof(thm_Arcref));
+	if (!row_count) return (thm_Digraph) { 0 };
+
+	thm_Digraph dg_out = thm_init_digraph(dg->vertices, dg->tail_ptr[dg->vertices]);
+	if (!dg_out.tail_ptr) {
+		free(row_count);
+		return dg_out;
+	}
+
+	for (thm_Vid* arc = dg->head;
+			arc != dg->head + dg->tail_ptr[dg->vertices];
+			++arc) {
+		++row_count[*arc + 1];
+	}
+
+	dg_out.tail_ptr[0] = 0;
+	for (thm_Vid v = 1; v <= dg->vertices; ++v) {
+		row_count[v] += row_count[v - 1];
+		dg_out.tail_ptr[v] = row_count[v];
+	}
+
+	for (thm_Vid v = 0; v < dg->vertices; ++v) {
+		for (thm_Vid* arc = dg->head + dg->tail_ptr[v];
+				arc != dg->head + dg->tail_ptr[v + 1];
+				++arc) {
+			dg_out.head[row_count[*arc]] = v;
+			++row_count[*arc];
+		}
+	}
+
+	free(row_count);
+
+	return dg_out;
+}
+
+
+thm_Digraph thm_digraph_undirected(const thm_Digraph* const dg) {
+	
+}
+
+
+thm_Digraph thm_digraph_adjacency_product(const thm_Digraph* const dg_a, const thm_Digraph* const dg_b, const bool force_diagonal, const bool ignore_diagonal) {
+	if (force_diagonal && ignore_diagonal) return (thm_Digraph) { 0 };
+	if (!dg_a || !dg_b || !dg_a->tail_ptr || !dg_b->tail_ptr) return (thm_Digraph) { 0 };
+	if (dg_a->vertices != dg_b->vertices) return (thm_Digraph) { 0 };
+	if (dg_a->vertices == 0) return thm_empty_digraph(0, 0);
+
+	thm_Vid* row_markers = malloc(sizeof(thm_Vid[dg_a->vertices]));
+	if (!row_markers) return (thm_Digraph) { 0 };
+
+	thm_Arcref out_max_arcs = 0;
+
+	// Greedy memory calculation first
+	for (thm_Vid v = 0; v < dg_a->vertices; ++v) {
+		if (force_diagonal) {
+			out_max_arcs += dg_b->tail_ptr[v + 1] - dg_b->tail_ptr[v];
+		}
+		for (thm_Vid* arc_a = dg_a->head + dg_a->tail_ptr[v];
+				arc_a != dg_a->head + dg_a->tail_ptr[v + 1];
+				++arc_a) {
+			if (*arc_a == v && (force_diagonal || ignore_diagonal)) continue;
+			out_max_arcs += dg_b->tail_ptr[*arc_a + 1] - dg_b->tail_ptr[*arc_a];
+		}
+	}
+
+	thm_Digraph dg_out = thm_init_digraph(dg_a->vertices, out_max_arcs);
+
+	if (!dg_out.tail_ptr) {
+		// Could not allocate digraph with `row_markers' arcs.
+		// Do correct (but slow) memory calculation and try again.
+
+		out_max_arcs = 0;
+		for (thm_Vid v = 0; v < dg_a->vertices; ++v) row_markers[v] = THM_VID_MAX;
+
+		for (thm_Vid v = 0; v < dg_a->vertices; ++v) {
+			if (force_diagonal) {
+				for (thm_Vid* arc_b = dg_b->head + dg_b->tail_ptr[v];
+						arc_b != dg_b->head + dg_b->tail_ptr[v + 1];
+						++arc_b) {
+					if (row_markers[*arc_b] != v) {
+						row_markers[*arc_b] = v;
+						++out_max_arcs;
+					}
+				}
+			}
+			for (thm_Vid* arc_a = dg_a->head + dg_a->tail_ptr[v];
+					arc_a != dg_a->head + dg_a->tail_ptr[v + 1];
+					++arc_a) {
+				if (*arc_a == v && (force_diagonal || ignore_diagonal)) continue;
+				for (thm_Vid* arc_b = dg_b->head + dg_b->tail_ptr[*arc_a];
+						arc_b != dg_b->head + dg_b->tail_ptr[*arc_a + 1];
+						++arc_b) {
+					if (row_markers[*arc_b] != v) {
+						row_markers[*arc_b] = v;
+						++out_max_arcs;
+					}
+				}
+			}
+		}
+
+		dg_out = thm_init_digraph(dg_a->vertices, out_max_arcs);
+	}
+
+	// Try again. If fail, give up.
+	if (!dg_out.tail_ptr) {
+		free(row_markers);
+		return dg_out;
+	}
+
+	dg_out.tail_ptr[0] = 0;
+	thm_Arcref out_arc_write = 0;
+	for (thm_Vid v = 0; v < dg_a->vertices; ++v) row_markers[v] = THM_VID_MAX;
+
+	for (thm_Vid v = 0; v < dg_a->vertices; ++v) {
+		if (force_diagonal) {
+			for (thm_Vid* arc_b = dg_b->head + dg_b->tail_ptr[v];
+					arc_b != dg_b->head + dg_b->tail_ptr[v + 1];
+					++arc_b) {
+				if (row_markers[*arc_b] != v) {
+					row_markers[*arc_b] = v;
+					dg_out.head[out_arc_write] = *arc_b;
+					++out_arc_write;
+				}
+			}
+		}
+		for (thm_Vid* arc_a = dg_a->head + dg_a->tail_ptr[v];
+				arc_a != dg_a->head + dg_a->tail_ptr[v + 1];
+				++arc_a) {
+			if (*arc_a == v && (force_diagonal || ignore_diagonal)) continue;
+			for (thm_Vid* arc_b = dg_b->head + dg_b->tail_ptr[*arc_a];
+					arc_b != dg_b->head + dg_b->tail_ptr[*arc_a + 1];
+					++arc_b) {
+				if (row_markers[*arc_b] != v) {
+					row_markers[*arc_b] = v;
+					dg_out.head[out_arc_write] = *arc_b;
+					++out_arc_write;
+				}
+			}
+		}
+
+		dg_out.tail_ptr[v + 1] = out_arc_write;
+	}
+
+	free(row_markers);
+
+	thm_change_digraph_head_size(&dg_out, out_arc_write);
+
+	return dg_out;
+}
+
+/*
+thm_Digraph thm_digraph_walks(const thm_Digraph* const dg, const thm_Vid length) {
+	return ithm_digraph_power_walk(dg, length, false);
+}
+
+thm_Digraph thm_digraph_power(const thm_Digraph* const dg, const thm_Vid exponent) {
+	return ithm_digraph_power_walk(dg, length, true);
+}
+
+thm_Digraph ithm_digraph_power_walk(const thm_Digraph* const dg, thm_Vid exponent, const bool power) {
+	if (!dg || !dg->tail_ptr) return (thm_Digraph) { 0 };
+	if (dg->vertices == 0) return thm_empty_digraph(0, 0);
+	if (exponent == 0) return thm_empty_digraph(dg->vertices, 0);
+	if (exponent == 1) return thm_copy_digraph(dg);
+	if (exponent == 2) return thm_digraph_adjacency_product(dg, dg, power, false);
+
+	thm_Digraph dg_tmp;
+	thm_Digraph dg_out = (thm_Digraph) { 0 };
+	thm_Digraph dg_expo = thm_copy_digraph(dg);
+	if (!dg_expo.tail_ptr) return (thm_Digraph) { 0 };
+
+	for (; exponent > 0; exponent /= 2) {
+		if (exponent % 2 == 1) {
+			if (!dg_out.tail_ptr) {
+				dg_out = thm_copy_digraph(&dg_expo);
+			} else {
+				dg_tmp = dg_out;
+				dg_out = thm_digraph_adjacency_product(&dg_tmp, &dg_expo);
+				thm_free_digraph(&dg_tmp);
+			}
+			if (!dg_out.tail_ptr) {
+				thm_free_digraph(&dg_expo);
+				return (thm_Digraph) { 0 };
+			} 
+		}
+		dg_tmp = dg_expo;
+		dg_expo = thm_digraph_adjacency_product(&dg_tmp, &dg_expo);
+		thm_free_digraph(&dg_tmp);
+		if (!dg_expo.tail_ptr) {
+			thm_free_digraph(&dg_out);
+			return (thm_Digraph) { 0 };
+		}
+	}
+
+	thm_free_digraph(&dg_expo);
+
+	return dg_out;
+}*/
+
--- digraph.h
+++ digraph.h
@@ -3,38 +3,53 @@
 
 #include <stdbool.h>
 #include <stddef.h>
-#include "config.h"
+#include <stdint.h>
+
+// Vertex ID type (must be unsigned)
+// # of vertices < MAX(thm_Vid)
+typedef uint32_t thm_Vid;
+
+// MAX(thm_Vid): maximum value of thm_Vid
+#define THM_VID_MAX UINT32_MAX
+
+// Arc reference type (must be unsigned)
+// # of arcs in any digraph <= MAX(thm_Arcref)
+typedef uint32_t thm_Arcref;
+
 
 typedef struct thm_Digraph thm_Digraph;
 struct thm_Digraph {
-	// Array of unsigned longs: [0, ..., max_arcs - 1].
-	// Contains the head of each arc, indicated by vertex ID.
-	thm_Vid* head;
-
-	// Array of pointers: [0, ..., vertices].
-	// For arcs for which vertex i is the tail, head[tail_ptr[i]] is the head of the first arc and head[tail_ptr[i+1] - 1] is the last arc.
-	thm_Arcref* tail_ptr;
-
 	// Number of vertices in graph: vertices < MAX(thm_Vid).
 	thm_Vid vertices;
 
 	// Maximum number of arcs in graph: max_arcs <= MAX(thm_Arcref).
 	thm_Arcref max_arcs;
 
-	// Valid and initialized (head may be NULL if max_arcs == 0)
-	bool valid;
+	// Array of unsigned longs: [0, ..., max_arcs - 1].
+	// Contains the head of each arc, indicated by vertex ID.
+	// May be NULL if max_arcs == 0.
+	thm_Vid* head;
+
+	// Array of pointers: [0, ..., vertices].
+	// For arcs for which vertex i is the tail, head[tail_ptr[i]] is the head of the first arc and head[tail_ptr[i+1] - 1] is the last arc.
+	// May never be NULL.
+	thm_Arcref* tail_ptr;
 };
+
 
 thm_Digraph thm_init_digraph(thm_Vid vertices, thm_Arcref max_arcs);
 void thm_free_digraph(thm_Digraph* dg);
 bool thm_change_digraph_head_size(thm_Digraph* dg, thm_Arcref new_max_arcs);
 thm_Digraph thm_empty_digraph(thm_Vid vertices, thm_Arcref max_arcs);
+thm_Digraph thm_identity_digraph(thm_Vid vertices);
 thm_Digraph thm_balanced_digraph(thm_Vid vertices, thm_Vid arcs_per_vertex, thm_Vid* heads);
-thm_Digraph thm_add_two_digraphs(const thm_Digraph* dg_a, const thm_Digraph* dg_b, bool remove_duplicates);
-thm_Digraph thm_add_digraphs(size_t num_dgs, const thm_Digraph** dgs, bool remove_duplicates);
-
-//fm_digraph fm_transpose(fm_digraph a);
-//fm_digraph fm_undirected(fm_digraph a);
-//fm_digraph fm_exclusion_graph(fm_digraph a);
+thm_Digraph thm_copy_digraph(const thm_Digraph* dg);
+thm_Digraph thm_digraph_union_two(const thm_Digraph* dg_a, const thm_Digraph* dg_b);
+thm_Digraph thm_digraph_union(size_t num_dgs, const thm_Digraph** dgs);
+thm_Digraph thm_digraph_transpose(const thm_Digraph* dg);
+thm_Digraph thm_digraph_undirected(const thm_Digraph* dg);
+thm_Digraph thm_digraph_adjacency_product(const thm_Digraph* dg_a, const thm_Digraph* dg_b, bool force_diagonal, bool ignore_diagonal);
+thm_Digraph thm_digraph_walks(const thm_Digraph* dg, thm_Vid length);
+thm_Digraph thm_digraph_power(const thm_Digraph* dg, thm_Vid exponent);
 
 #endif
