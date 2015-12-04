356c9fc235a168b84603ebda7cb8f60e8a9e2088
Fredrik Sävje <fred_gh@savje.com>
2015-12-03 21:10:51 -0800

    Add new license format to tests

--- tests/test_suite.h
+++ tests/test_suite.h
@@ -1,21 +1,21 @@
-/*  scclust -- A C library for size constrained clustering
-    https://github.com/fsavje/scclust
-
-    Copyright (C) 2015  Fredrik Savje -- http://fredriksavje.com
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+/* scclust -- A C library for size constrained clustering
+ * https://github.com/fsavje/scclust
+ *
+ * Copyright (C) 2015  Fredrik Savje -- http://fredriksavje.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ==============================================================================*/
 
 
@@ -28,13 +28,14 @@
 #include <cmocka.h>
 
 #include <stdlib.h>
+#include <assert.h>
+
 #define malloc(size) _test_malloc(size, __FILE__, __LINE__)
 #define calloc(num, size) _test_calloc(num, size, __FILE__, __LINE__)
 #define realloc(ptr, size) _test_realloc(ptr, size, __FILE__, __LINE__)
 #define free(ptr) _test_free(ptr, __FILE__, __LINE__)
 
-#include <assert.h>
 #undef assert
-#define assert(expression) mock_assert((int)(expression), #expression, __FILE__, __LINE__)
+#define assert(expression) mock_assert((int)(expression), #expression, __LINE__)
 
 #endif
