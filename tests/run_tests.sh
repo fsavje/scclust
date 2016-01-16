d62371ae736e1afc29bd6ec85db905f697cd60e0
Fredrik Sävje <fred_gh@savje.com>
2016-01-15 18:18:20 -0800

    Add tests for caliper clustering

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -16,6 +16,7 @@ run_test test_digraph_core
 run_test test_digraph_debug
 run_test test_digraph_operations
 run_test test_findseeds
+run_test test_nng_caliper
 run_test test_nng_clustering
 run_test test_ann_wrapper
 run_test test_findseeds_internals
