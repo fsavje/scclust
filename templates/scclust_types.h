84554b61ec1fb7f1edbe8fe77a744e2a95d8c44e
Fredrik Savje <fred_gh@savje.com>
2017-01-18 01:20:20 -0800

    Fix type comparison bug

--- templates/scclust_types.h
+++ templates/scclust_types.h
@@ -50,6 +50,7 @@ typedef {% arcindex_type %} iscc_ArcIndex;
 #define ISCC_M_ARCINDEX_TYPE_{% arcindex_type %}
 
 static const scc_Clabel SCC_CLABEL_MAX = {% clabel_max %};
+static const scc_PointIndex ISCC_POINTINDEX_MAX_PI = {% pointindex_max %};
 static const uintmax_t ISCC_POINTINDEX_MAX = {% pointindex_max %};
 static const uintmax_t ISCC_ARCINDEX_MAX = {% arcindex_max %};
 static const uintmax_t ISCC_TYPELABEL_MAX = 65535;
